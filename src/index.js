2b20181edae930a8f4bb50ff26ec117cb41e11be
Ivan <reva.ivan2012@gmail.com>
2026-05-08 13:12:11 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const box = [];
+
+  const pairs = {};
+  const opens = new Set();
+  const same = new Set();
+
+  bracketsConfig.forEach(([openBracket, closeBracket]) => {
+    pairs[closeBracket] = openBracket;
+    opens.add(openBracket);
+
+    if (openBracket === closeBracket) {
+      same.add(openBracket);
+    }
+  });
+
+  const valid = str.split('').every((symbol) => {
+    if (same.has(symbol)) {
+      if (box[box.length - 1] === symbol) {
+        box.pop();
+      } else {
+        box.push(symbol);
+      }
+
+      return true;
+    }
+
+    if (opens.has(symbol)) {
+      box.push(symbol);
+      return true;
+    }
+
+    const last = box.pop();
+
+    return last === pairs[symbol];
+  });
+
+  return valid && box.length === 0;
 };
